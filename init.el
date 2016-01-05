036d3dfbaa8e178f690d63894b10d88a5a6883f2
Bruce Durling <bruce@mastodonc.com>
2016-01-05 19:10:35 +0000

    Platform and display tweaks

--- init.el
+++ init.el
@@ -65,5 +65,72 @@
   :pin melpa-stable
   :defer t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Display Tweaking
 (load-theme 'wheatgrass)
+
+;; no toolbar
+(tool-bar-mode -1)
+
+;; no scroll bar
+(scroll-bar-mode -1)
+
+;; no horizontal scroll bar
+(when (boundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
+
+;; show line and column numbers
+(line-number-mode 1)
+(column-number-mode 1)
+
+;; This really ought to be the default
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Other useful defaults
+
+;; keep autobackups under control
+(setq
+ backup-by-copying t      ; don't clobber symlinks
+ backup-directory-alist
+ '(("." . "~/.saves"))    ; don't litter my fs tree
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)       ; use versioned backups
+
+;; put custom stuff in a different file
+(setq custom-file (concat user-emacs-directory "custom.el"))
+
+;; uniquify buffers with the same file name but different actual files
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward)
+
+;; Delete that horrible trailing whitespace
+(add-hook 'before-save-hook
+          (lambda nil
+            (delete-trailing-whitespace)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Platform specific stuff
+
+;; No # on UK Macs
+(when (memq window-system '(mac ns))
+  (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#"))))
+
+;; Handle unicode better
+(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
+
+;; get the path from shell
+(use-package exec-path-from-shell
+  :ensure t
+  :defer t
+  :pin melpa-stable
+  :config (exec-path-from-shell-initialize))
+
+;; fix yer speling
+(when (memq window-system '(mac ns))
+  (setq ispell-program-name (executable-find "aspell")))
+
+;; Handle unicode better
+(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
